c2d61fad471abbb7289fbaaac41ddc852d27b788
winstr <seunghyeon.me@gmail.com>
2023-09-23 11:47:14 +0000

    First commit.

---
+++ lib/ch8_3_statelesswidget/stateless_test.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  bool enabled = false;
+  String stateText = "disable";
+
+  void changeCheck() {
+    if (enabled) {
+      stateText = "disable";
+      enabled = true;
+    } else {
+      stateText = "enable";
+      enabled = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Stateless Text'),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: (enabled
+                  ? Icon(
+                      Icons.check_box,
+                      size: 20,
+                    )
+                  : Icon(
+                      Icons.check_box_outline_blank,
+                      size: 20,
+                    )),
+              color: Colors.red,
+              onPressed: changeCheck,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 16),
+              child: Text(
+                '$stateText',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
